ec765d4c016f81ba189af3ee088a3494b890712b
Bruno Sesso <bruno.sesso@gmail.com>
2020-09-20 16:52:27 -0300

    Referencing user posts

--- src/main/java/br/com/brunosesso/workshopmongo/dto/UserDTO.java
+++ src/main/java/br/com/brunosesso/workshopmongo/dto/UserDTO.java
@@ -1,5 +1,11 @@
 package br.com.brunosesso.workshopmongo.dto;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.data.mongodb.core.mapping.DBRef;
+
+import br.com.brunosesso.workshopmongo.domain.Post;
 import br.com.brunosesso.workshopmongo.domain.User;
 
 public class UserDTO {
@@ -7,6 +13,9 @@ public class UserDTO {
 	private String id;
 	private String name;
 	private String email;
+	
+	@DBRef(lazy = true)
+	private List<Post> posts = new ArrayList<>();
 	
 	public UserDTO() {
 	}
@@ -40,6 +49,38 @@ public class UserDTO {
 	public void setEmail(String email) {
 		this.email = email;
 	}
-	
+
+	public List<Post> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		UserDTO other = (UserDTO) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
 	
 }
